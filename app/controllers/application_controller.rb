97eee8327a25b7bdbd02932e085ad5d773d7124c
BugsForDays <jmslca123@gmail.com>
2018-12-27 21:09:19 -0800

    implement rough layout of user dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
       user.permit(:name, :email, :state, :country, :birthday, :password, :password_confirmation, :referral)
     end
   end
+
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', sign_up: 'register' }
   root 'welcome#index'
-
-  devise_scope :user do
-    get 'login', to: 'devise/sessions#new'
-    get 'register', to: 'devise/registrations#new'
-    post 'register', to: 'devise/registrations#create'
-    delete 'logout', to: 'devise/sessions#destroy'
-    # get  "users/confirmation/new", to: "devise/confirmations#new", as: "confirmation"
-  end
-
+  
+  get '/dashboard' => 'users#show'
   get '/themes' => 'themes#index'
   resources :tips_daily, only: [:index, :show]
 end
