33fa0f6ca2db199f01e6e7740b5fce40bb6e99d0
BugsForDays <jmslca123@gmail.com>
2019-02-09 18:37:01 -0800

    update rspec

--- spec/controllers/challenges_controller_spec.rb
+++ spec/controllers/challenges_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe ChallengesController, type: :controller do
   describe "GET #show" do
     it "returns http success" do
       get :show
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(404)
     end
   end
 
