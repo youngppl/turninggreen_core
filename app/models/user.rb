1ca129e3aeed33baf221394e6addfe6fa7d9cd24
petabite <jmslca123@gmail.com>
2019-04-19 15:29:41 -0700

    fix popup box sizing

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ApplicationRecord
 
   def validate_age
     if (!birthday.nil?) and (birthday > 13.years.ago)
-      errors.add(:base, 'You must be at least 13 years of age')
+      errors.add(:base, 'You must be at least 13 years of age') 
     end
   end
 
