b2d05aaf44cf8ec1002e8debfab466f3276c1c08
petabite <jmslca123@gmail.com>
2019-06-08 19:10:00 -0700

    fix test

--- db/migrate/20190608225551_add_reflection_completed_to_challenges.rb
+++ db/migrate/20190608225551_add_reflection_completed_to_challenges.rb
@@ -1,6 +1,5 @@
 class AddReflectionCompletedToChallenges < ActiveRecord::Migration[5.2]
   def change
     add_column :challenges, :reflection_completed, :boolean, default: false
-    add_column :challenges, :date_complete, :date
   end
 end
