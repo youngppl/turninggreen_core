c100d5082dbf58ba16731f0844ab1517722a7874
petabite <jmslca123@gmail.com>
2019-06-21 16:16:18 -0700

    add uncompleted challenges to the completed challenges page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def getUserCompletedChallengeNames
+    challengeNames = []
+    current_user.challenges.each { |challenge| challengeNames << challenge.challenge_name }
+    return challengeNames
+  end
 end
