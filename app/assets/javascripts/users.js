ecee17a0e64f29f34d2dc6db040a97bf088db0b4
petabite <jmslca123@gmail.com>
2019-07-03 21:48:30 -0700

    finish implementation of the settings profile tab

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -7,7 +7,26 @@ $(window).on('turbolinks:load', function(){
     html: true,
     template: '<div class="popover no_challenge_popover" role="tooltip"><div class="arrow"></div><div class="border-arrow arrow" style="top: 43px;"></div><h3 class="popover-header"></h3><button type="button" class="close">&times;</button><div class="popover-body"></div></div>'
   }).popover('show');
+  $("#user_email").data("original", $("#user_email").val());
+  $("#user_state").data("original", $("#user_state").val());
+  $("#user_country").data("original", $("#user_country").val());
 });
 $(document).on("click", ".popover .close" , function(){
         $(this).parents(".popover").popover('hide');
 });
+
+function checkEmail() {
+  if (!document.getElementById("user_email").validity.valid || $("#user_email").val() == "") {
+    $(".email.error").show();
+  } else {
+    $(".email.error").hide();
+  }
+}
+
+function allowSaveButton() {
+  if ((($("#user_email").data("original") != $("#user_email").val()) || ($("#user_state").data("original") != $("#user_state").val()) || ($("#user_country").data("original") != $("#user_country").val())) && document.getElementById("user_email").validity.valid) {
+    $("#profile-save-button").prop("disabled", false);
+  } else {
+    $("#profile-save-button").prop("disabled", true);
+  }
+}
