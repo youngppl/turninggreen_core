6c5a84046bde5cef4fd3e37d0b8ee03b94b2e56d
petabite <jmslca123@gmail.com>
2019-07-05 22:43:39 -0700

    changes saved and account deactivation popups

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -30,3 +30,27 @@ function allowSaveButton() {
     $("#profile-save-button").prop("disabled", true);
   }
 }
+
+function checkDeactivateForm() {
+  if (document.getElementById("other").checked) {
+    $("#other-textarea").prop("disabled", false);
+    $(".deactivate.error").show();
+  } else {
+    $("#other-textarea").prop("disabled", true);
+    $("#other-textarea").val("");
+  }
+  
+  if ((document.getElementById("other").checked && $("#other-textarea").val().length > 6) || document.getElementById("too-much-work").checked || document.getElementById("not-safe").checked || document.getElementById("not-useful").checked) {
+    $("#deactivate-next-button").prop("disabled", false);
+  } else {
+    $("#deactivate-next-button").prop("disabled", true);
+  }
+
+  if (document.getElementById("other").checked && $("#other-textarea").val().length < 6) {
+    $(".deactivate.error").show();
+    $("#deactivate-next-button").prop("disabled", true);
+  } else {
+    $(".deactivate.error").hide();
+  }
+
+}
