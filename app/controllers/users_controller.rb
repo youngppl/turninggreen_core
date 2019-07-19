eea6b985f186b85c144d2d3d980adab848bd054e
petabite <jmslca123@gmail.com>
2019-07-19 16:58:16 -0700

    fix notifications bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     # user dashboard, redirects here after successful login
     current_user.challenges.all.each do |challenge|
       if challenge.date_complete <= Time.now.utc
-        challenge.update(completed:true)
+        challenge.update(completed:true, notification_viewed:true)
       end
     end
     @current_challenges = current_user.challenges.where(completed:false)
