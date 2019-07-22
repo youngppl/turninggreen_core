a9d2e5b17555ccda087dd4bc81c1b73cd38f5146
petabite <jmslca123@gmail.com>
2019-07-21 21:09:59 -0700

    settings page and registrations updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user|
-      user.permit(:name, :email, :state, :country, :birthday, :password, :password_confirmation, :referral)
+      user.permit(:name, :email, :state, :country, :birthday, :password, :password_confirmation, :referral, :permissions => [])
     end
     devise_parameter_sanitizer.permit(:account_update) do |user|
       user.permit(:email, :state, :country, :current_password, :password, :password_confirmation, :timezone, :profile_pic, :notifications, :notifications_content => [], :permissions => [])
