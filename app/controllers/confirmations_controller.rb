cfb4b96c235936830f55eaeb6c93f7d622129ba6
petabite <jmslca123@gmail.com>
2019-07-24 21:07:33 -0700

    fix after email confirmation redirect path

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,8 +1,16 @@
 class ConfirmationsController < Devise::ConfirmationsController
-  
+
   protected
 
   def after_resending_confirmation_instructions_path_for(resource)
     edit_user_registration_path
   end
+
+  def after_confirmation_path_for(resource_name, resource)
+      if signed_in?(resource_name)
+        dashboard_path
+      else
+        new_session_path(resource_name)
+      end
+    end
 end
