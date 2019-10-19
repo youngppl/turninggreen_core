4559a7f34908630c1661fc65a617a06d87557441
petabite <jmslca123@gmail.com>
2019-10-19 14:17:46 -0700

    checkpoint

--- app/assets/javascripts/reflections.js
+++ app/assets/javascripts/reflections.js
@@ -1,8 +1,3 @@
-function loadUpload(event) {
-  var getImagePath = URL.createObjectURL(event.target.files[0]);
-  $(event.target).parent().parent().css('background-image', 'url(' + getImagePath + ')');
-}
-
 function submitReflection(self, challenge_id) {
   parent = $(self).parentsUntil('.panel')
   error = false
@@ -31,4 +26,90 @@ function submitReflection(self, challenge_id) {
     $(self).parents('.panel').hide();
     $('#points-earned-'+challenge_id).modal();
   }
+}
+
+// function loadUpload(event) {
+//   var getImagePath = URL.createObjectURL(event.target.files[0]);
+//   $(event.target).parent().parent().css('background-image', 'url(' + getImagePath + ')');
+// }
+
+var $uploadCrop, rawImg, photo_input;
+
+function blobToFile(theBlob, fileName){
+    theBlob.lastModifiedDate = new Date();
+    theBlob.name = fileName;
+    return theBlob;
+}
+
+function readFile(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      $uploadCrop.croppie("bind", {
+          url: e.target.result
+        });
+    };
+    reader.readAsDataURL(input.files[0]);
+  } else {
+    alert("Sorry, your browser doesn't support the FileReader API");
+  }
+}
+
+// $(window).on('shown.bs.modal', function(e) {
+function linkCroppie(input) {
+    photo_input = input
+    readFile(input);
+    container = $(input).siblings('.modal').find('.modal-body')
+    container.css('display', 'block')
+    container.siblings('.modal-footer').find('.upload').addClass('green')
+    container.siblings('.modal-footer').find('.upload').prop('disabled', false)
+    container.find('.browse').hide()
+    $uploadCrop = container.croppie({
+      enableExif: true,
+      url: rawImg,
+      showZoomer: false,
+      viewport: {
+        width: 240,
+        height: 240,
+        type: 'square'
+      }
+    });
+}
+
+// $(window).on('hidden.bs.modal', function(e) {
+//   if(e.target==$('#pic-input-dialog')[0]){
+//     $('#upload-croppie').croppie('destroy');
+//   }
+// });
+
+
+function updatePicture() {
+  $uploadCrop.croppie("result", {
+      type: 'blob'
+    }).then(function(blob) {
+        // reader = new FileReader()
+        // reader.readAsDataURL(blob)
+        $(photo_input).siblings('#upload-hidden-field').val(new File([blob], 'pic-upload.png'))
+        tmp_url = URL.createObjectURL(blob)
+        $(photo_input).parent().css('background-image', 'url(' + tmp_url + ')');
+        // "use strict";
+
+        // var input = document.querySelector('input[type=file]');
+        // var url = input.dataset.directUploadUrl;
+        // var upload = new ActiveStorage.DirectUpload(blobToFile(blob, "pic-upload.png"), url);
+        //
+        // upload.create(function (error, blob) {
+        //   if (error) {
+        //     alert(error);
+        //   } else {
+        //     var hiddenField = document.createElement('input');
+        //     hiddenField.setAttribute("type", "hidden");
+        //     hiddenField.setAttribute("value", blob.signed_id);
+        //     hiddenField.name = input.name;
+        //     document.querySelector('form').appendChild(hiddenField);
+        //     document.querySelector('form').submit();
+        //   }
+        // });
+
+    });
 }
--- app/assets/javascripts/registrations.js
+++ app/assets/javascripts/registrations.js
@@ -7,84 +7,10 @@ function save_timezone() {
   document.cookie = 'timezone' + '=' + tz;
 }
 
-function show_filename() {
-  var fileInput = document.getElementsByClassName('upload-btn')[0];
-  document.getElementById("uploadFile").innerHTML = fileInput.files[0].name;
-}
-
-var $uploadCrop, rawImg;
-
-function blobToFile(theBlob, fileName){
-    theBlob.lastModifiedDate = new Date();
-    theBlob.name = fileName;
-    return theBlob;
-}
-
-function readFile(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-    reader.onload = function(e) {
-      $uploadCrop.croppie("bind", {
-          url: e.target.result
-        });
-    };
-    reader.readAsDataURL(input.files[0]);
-  } else {
-    alert("Sorry, your browser doesn't support the FileReader API");
-  }
-}
-
-$(window).on('shown.bs.modal', function(e) {
-  if(e.target==$('#pic-input-dialog')[0]){
-    readFile(document.getElementById('upload-photo-input'));
-    $uploadCrop = $('#upload-croppie').croppie({
-      enableExif: true,
-      url: rawImg,
-      viewport: {
-        width: 200,
-        height: 200,
-        type: 'circle'
-      },
-      boundary: {
-        width: 300,
-        height: 300
-      }
-    });
-  }
-});
-
-$(window).on('hidden.bs.modal', function(e) {
-  if(e.target==$('#pic-input-dialog')[0]){
-    $('#upload-croppie').croppie('destroy');
-  }
-});
-
-
-function updatePicture() {
-  $uploadCrop.croppie("result", {
-      type: 'blob',
-      circle: true
-    }).then(function(blob) {
-        "use strict";
-        var input = document.querySelector('input[type=file]');
-        var url = input.dataset.directUploadUrl;
-        var upload = new ActiveStorage.DirectUpload(blobToFile(blob, "pic-upload.png"), url);
-
-        upload.create(function (error, blob) {
-          if (error) {
-            alert(error);
-          } else {
-            var hiddenField = document.createElement('input');
-            hiddenField.setAttribute("type", "hidden");
-            hiddenField.setAttribute("value", blob.signed_id);
-            hiddenField.name = input.name;
-            document.querySelector('form').appendChild(hiddenField);
-            document.querySelector('form').submit();
-          }
-        });
-
-    });
-}
+// function show_filename() {
+//   var fileInput = document.getElementsByClassName('upload-btn')[0];
+//   document.getElementById("uploadFile").innerHTML = fileInput.files[0].name;
+// }
 
 function convertDate() {
   date = new Date($('#datepicker').val());
