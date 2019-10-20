467d6b669af84ac3ef9a75781ade55c3d6a21102
尸廾工L工尸 乙 <jmslca123@gmail.com>
2019-10-19 18:52:33 -0700

    Challenge Email Reminders (#69)
    * implement base email bodies
    
    * implement automated challenge reminder system

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_23_000548) do
+ActiveRecord::Schema.define(version: 2019_10_19_224930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,6 +106,7 @@ ActiveRecord::Schema.define(version: 2019_09_23_000548) do
     t.integer "points", default: 0
     t.integer "level", default: 0
     t.boolean "admin", default: false
+    t.string "emails_sent", default: [], array: true
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
