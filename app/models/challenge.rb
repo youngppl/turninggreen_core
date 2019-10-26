484e4c4f3872dc19043f8e5c3f340d29d5d321be
petabite <jmslca123@gmail.com>
2019-10-26 16:33:54 -0700

    update challenge ending timeframe

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -14,14 +14,14 @@ class Challenge < ApplicationRecord
   end
 
   def get_time_left
-    @weeks = (date_complete - DateTime.now.getutc) / 60 / 60 / 24 / 7
+    @weeks = (date_complete.getutc.to_date - DateTime.now.getutc.to_date) / 7
     if @weeks < 1
-      @days = (date_complete - DateTime.now.getutc) / 60 / 60 / 24
-      if @days < 1
-        pluralize ((date_complete - DateTime.now.getutc) / 60 / 60).round, 'hour'
-      else
-        pluralize @days.round, 'day'
-      end
+      @days = (date_complete.getutc.to_date - DateTime.now.getutc.to_date)
+      # if @days < 1
+      #   pluralize ((date_complete.to_date - DateTime.now.getutc.to_date) / 60 / 60).round, 'hour'
+      # else
+      pluralize @days.round, 'day'
+      # end
     else
       pluralize @weeks.round, 'week'
     end
