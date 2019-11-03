27e2529c485fe7f260f0ae42ec4e91cfb75362e9
petabite <jmslca123@gmail.com>
2019-11-02 19:24:32 -0700

    clean up

--- app/controllers/challenge_mailer_controller.rb
+++ app/controllers/challenge_mailer_controller.rb
@@ -18,22 +18,6 @@ class ChallengeMailerController < ApplicationController
         (user.notifications == "Every other day" && Date.today.day % 2 == 0)
           ChallengeMailer.challenge_reminder_email(user, updates, facts.sample).deliver_later
       end
-
-      # if user.emails_sent.length < 2
-      #   if !user.emails_sent.include?('completed') and !user.challenges.where(completed: true, reflection_completed: false, date_complete: 1.week.ago..Date.today).empty?
-      #     ChallengeMailer.challenge_completed_email(user).deliver_later
-      #     user.emails_sent << 'completed'
-      #     user.save!
-      #   elsif !user.emails_sent.include?('ending') and !user.challenges.where(completed: false, date_complete: Date.today..3.days.from_now).empty?
-      #     ChallengeMailer.challenge_ending_soon_email(user).deliver_later
-      #     user.emails_sent << 'ending'
-      #     user.save!
-      #   elsif !user.emails_sent.include?('progress') and !user.challenges.all.empty?
-      #     ChallengeMailer.challenge_in_progress_email(user).deliver_later
-      #     user.emails_sent << 'progress'
-      #     user.save!
-      #   end
-      # end
     end
   end
 
