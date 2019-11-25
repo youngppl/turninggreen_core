974ef3565ee47161cae13cd7e308863665346ce7
petabite <jmslca123@gmail.com>
2019-11-25 12:25:05 -0800

    fix navbar bugs

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -2,6 +2,13 @@ $(document).on('turbolinks:load', function() {
   $('.notifications-dropdown').on('click', function(e) {
     e.stopPropagation();
   })
+  window.addEventListener('resize', function(event){
+    if ($(document).width() > 1250) {
+      $('.content-col').removeClass('nav-toggled')
+    } else if ($('.dash-nav').hasClass('nav-toggled')) {
+      $('.content-col').addClass('nav-toggled')
+    }
+  });
 });
 
 function updateNotification(id) {
