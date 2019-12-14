16543082bbdc63b9d7e3ebffe299195e9a6f5568
petabite <jmslca123@gmail.com>
2019-12-14 15:25:36 -0800

    fix design bugs

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,6 +1,18 @@
+themes = ["waste", "body", "fashion", "water", "food", "biodiversity", "clean", "wellness", "home", "travel"]
+
 function toggleChallengePreview(theme) {
   $('.icon').removeClass('active')
   $('.challenge-box').hide()
   $('.challenge-box.' + theme).show()
   $('.icon.' + theme).addClass('active')
+  current_preview = themes.indexOf(theme)
+}
+
+function nextChallengePreview() {
+  if (current_preview == 9) {
+    current_preview = 0
+  } else {
+    current_preview += 1
+  }
+  toggleChallengePreview(themes[current_preview])
 }
