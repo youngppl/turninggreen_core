74cfa2fd85d765939e8bd35e76575cd1a2e5dee5
petabite <jmslca123@gmail.com>
2020-01-29 19:54:45 -0800

    try to fix errors

--- db/migrate/20200114052542_update_permsissions_for_users.rb
+++ db/migrate/20200114052542_update_permsissions_for_users.rb
@@ -1,6 +1,6 @@
 class UpdatePermsissionsForUsers < ActiveRecord::Migration[5.2]
   def change
     change_column :users, :permissions, :string, default: ["location"], array: true
-    User.update_all(:permissions => ["location"])
+    User.update_all(permissions: ["location"])
   end
 end
