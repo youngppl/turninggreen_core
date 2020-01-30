8de29f503b54a58820481fedc12510cf5257f5e1
petabite <jmslca123@gmail.com>
2020-01-29 19:59:00 -0800

    fix error

--- db/migrate/20200114052542_update_permsissions_for_users.rb
+++ db/migrate/20200114052542_update_permsissions_for_users.rb
@@ -1,6 +1,8 @@
 class UpdatePermsissionsForUsers < ActiveRecord::Migration[5.2]
   def change
     change_column :users, :permissions, :string, default: ["location"], array: true
-    User.update_all(permissions: ["location"])
+    if !Rails.env.test?
+      User.update_all(permissions: ["location"])
+    end
   end
 end
