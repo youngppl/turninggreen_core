188f0d61d33334030b7cb3d9878dc86f85c6dcc8
petabite <jmslca123@gmail.com>
2020-02-06 21:50:33 -0800

    implement points earned page

--- app/assets/javascripts/mobile/dashboard.js
+++ app/assets/javascripts/mobile/dashboard.js
@@ -1,4 +1,9 @@
 function updateActiveButton(el) {
+  // toggle button active states
   $('.menu').find('button').removeClass('active')
   $(el).addClass('active')
+  // show respective tab content
+  $('.content').find('.tab').removeClass('active')
+  target = $(el).data('target')
+  $('.content').find('.' + target).addClass('active')
 }
