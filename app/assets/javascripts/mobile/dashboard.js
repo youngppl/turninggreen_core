f9c8e4316c6b920aaf56fcdab6bdc6d6ea266a9a
petabite <jmslca123@gmail.com>
2020-02-15 11:46:05 -0800

    basic layout for log progress partial

--- app/assets/javascripts/mobile/dashboard.js
+++ app/assets/javascripts/mobile/dashboard.js
@@ -7,3 +7,19 @@ function updateActiveButton(el) {
   target = $(el).data('target')
   $('.content').find('.' + target).addClass('active')
 }
+
+function enableLogButton(el) {
+  if ($(el).val()) {
+    $('.next-button .disabled').hide()
+    $('.next-button .active').show()
+  } else {
+    $('.next-button .disabled').show()
+    $('.next-button .active').hide()
+  }
+}
+
+function logProgress(el) {
+  if ($(el).children('.next-button .active').is(':visible')) {
+    // if not disabled
+  }
+}
