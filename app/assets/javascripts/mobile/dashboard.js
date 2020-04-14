655a8c3dbbf72c30714f043396b9037236e6c197
petabite <jmslca123@gmail.com>
2020-04-13 19:21:08 -0700

    finish progress logging flow

--- app/assets/javascripts/mobile/dashboard.js
+++ app/assets/javascripts/mobile/dashboard.js
@@ -26,16 +26,23 @@ function enableLogButton(el) {
 }
 
 function logProgress(el, challenge_id) {
+  log_metric = $(el).siblings('.input').val()
   if ($(el).children('.next-button .active').is(':visible')) {
     // if button not disabled -> log progress
     $.post('/logs/new', {
       challenge_id: challenge_id,
-      metric: $(el).siblings('.input').val()
+      metric: log_metric
     });
     // hide prompt
     $(el).parent().hide()
     // mark challenge card as logged
     $(el).parent().prev('.carousel-item').addClass('logged')
+    //toggle challenge card view
+    challenge_card = $('.challenge-card#' + challenge_id)
+    challenge_card.find('.logged').removeClass('d-none')
+    challenge_card.find('.logged').css('display', 'flex')
+    challenge_card.find('.not-logged').hide()
+    challenge_card.find('.logged .todays-log').val(log_metric)
     // update and show metric popup
     eval("updateChart_"+challenge_id+"()")
     // show points
