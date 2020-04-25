1c7901eb064d8b9f2620400cd6f5065ef5c2babb
petabite <jmslca123@gmail.com>
2020-04-24 18:30:36 -0700

    pre-challenge flow

--- app/assets/javascripts/mobile/challenges.js
+++ app/assets/javascripts/mobile/challenges.js
@@ -1,15 +1,27 @@
 function toggleSlide() {
-    $(this).find('.carousel-indicators').hide()
-    $(this).find('.controls').css('display', 'flex')
     // toggle slider if swipe slid
     index = $(this).find('.carousel-item.active').index()
+    if (index == 0) {
+        $(this).find('.controls').hide()
+        $(this).find('.back.button').hide()
+        $(this).find('.done.button').hide()
+        $(this).find('.start.button').hide()
+        $(this).find('.next.button').show()
+    }
     if (index == 1 || index == 2) {
-        $(this).find('.handle').toggleClass('toggled')
+        $(this).find('.controls').css('display', 'flex')
+        $(this).find('.back.button').show()
+        $(this).find('.done.button').show()
+        $(this).find('.start.button').show()
+        $(this).find('.next.button').hide()
     }
     if (index == 1) {
-        $(this).find('.slide span').replaceWith('<span class="text-right">Tap to go back</span>')
-    } else if (index == 2) {
+        $(this).find('.handle').removeClass('toggled')
         $(this).find('.slide span').replaceWith('<span>Tap to learn more</span>')
+    } else if (index == 2) {
+        $(this).find('.handle').addClass('toggled')
+        $(this).find('.slide span').replaceWith('<span class="text-right">Tap to go back</span>')
+        $(this).find('.carousel-indicators li').eq(1).addClass('active')
     }
 }
 
@@ -38,15 +50,14 @@ function startChallenge(el) {
 }
 
 function startPreChallenge(el) {
-    var d = new Date();
-    end_date = new Date(Date.now() + (6.04e+8 * 4))
     challenge_name = $(el).parents('.challenge-popup').data('challenge_name')
     $.post('/challenges/add', {
         challenge_name: challenge_name,
         theme: challengeName,
         length_of_challenge: 0,
-        date_complete: end_date,
-        completed: false
+        date_complete: new Date(Date.now())
+,
+        completed: true
     })
-    $('#challenge-points').show()
+    $('#pre-challenge-points').show()
 }
